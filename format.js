41d37f2890be67e29dab0d5ef49fc7cc778b3098
layah <layah66@126.com>
2018-03-07 17:45:21 +0800

    Update format.js

--- format.js
+++ format.js
@@ -1,11 +1,12 @@
- ;(function(global,fn){
-        'use strict';
+(function(global,fn){
         if(!global.format){
-            return global.format=fn();
+            return global.format=fn;
         };
-       })(window,function(){
-        function DateFomat(date,fmt){
-                this.date=(date==undefined?new Date():date);
+        return global.format;
+       })(window,function(date,fmt){
+       	'use strict';
+        function DateFomat(){
+                this.date=(!date?new Date():date);
                 this.default=fmt || "yyyy MM dd HH:mm:ss w";
                 var pad=function(val){
                     var len=2;
@@ -17,7 +18,7 @@
                 };
 
                 var dayName=function(index){
-                    var dateNames=['星期日','星期一','星期二','星期三','星期四','星期五','星期六',];
+                    var dateNames=['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
                     return dateNames[index];
                 };
 
@@ -49,11 +50,14 @@
                 }
             };
             DateFomat.prototype.format=function(fmtDate){
+            	console.time()
                 var parse=/yy(?:yy)|d{2}|MM|HH|[aA]|w+|[yMdHmsw]+|"[^"]*"|'[^']*'/g;
                 var _this=this;
+                
                 return this.default.toString().replace(parse,function($0,$1,$2){
                     return $0 in fmtDate && fmtDate[$0](_this.date);
                 });
+                console.timeEnd()
             }; 
-            return DateFomat;
+            return new DateFomat(date,fmt);
        });
